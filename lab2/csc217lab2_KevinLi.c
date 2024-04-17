7f6e4d15d9c154b70a938a106f4b79d678b43080
kev556 <26943287+kev556@users.noreply.github.com>
2024-04-17 17:46:29 -0400

    Introduced interpretation of cli args

--- lab2/csc217lab2_KevinLi.c
+++ lab2/csc217lab2_KevinLi.c
@@ -2,35 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mystrlen.h"
-#include "freeall.h"
+#include "enterInput.h"
 
-int main(void) {
-    char *arr[5];
-    int i = 0, max = 0, maxidx = 0;
-
-    for (i = 0; i < 5; i++) {
-        char temp[102];
-        fgets(temp, 102, stdin);
-        temp[mystrlen(temp) - 2] = '\0';
-
-        arr[i] = malloc(102);
-        strncpy(arr[i], temp, 102);
-
-        int n = mystrlen(arr[i]) - 1;
-        if (n > max) {
-            max = n;
-            maxidx = i;
-        }
-    }
-    for (i = 0; i < 5; i++)
-        printf("%s\n", arr[i]);
-
-    printf("The largest string entered is [ %s ] with a length of %d \n", arr[maxidx], max);
-
-    freeAll(arr , 5);
-
-    for (i = 0; i < 5; i++)
-        printf("%s\n", arr[i]);
-
-    return 0;
+int main(int argc, char **argv) {
+    if (argc == 1)
+        enterInput();
+    else if (argc == 6)
+        cliInput(argv);
 }
