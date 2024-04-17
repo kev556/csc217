d2624c1d69ec0edccc1395bb415907abad033a7e
kev556 <26943287+kev556@users.noreply.github.com>
2024-04-17 18:19:52 -0400

    added error message for improper # cli args

--- lab2/csc217lab2_KevinLi.c
+++ lab2/csc217lab2_KevinLi.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-#include "enterInput.h"
-#include "cliInput.h"
+#include "user.h"
+#include "cli.h"
 
 int main(int argc, char **argv) {
+    char **arr;
+    int maxidx = 0, max = 0;
+
     if (argc == 1)
-        enterInput();
+        arr = enterInput(&max, &maxidx);
     else if (argc == 6)
-        cliInput(argv);
+        arr = cliInput(argc - 1, argv, &max, &maxidx);
+    else {
+        fprintf(stderr, "Incorrect number of cli arguments, please enter 5 strings.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < 5; i++)
+        printf("%s\n", arr[i]);
+
+    printf("The largest string entered is [ %s ] with a length of %d \n", arr[maxidx], max);
+
+    for (int i = 0; i < 5; i++)
+        free(arr[i]);
+    
+    return 0;
 }
