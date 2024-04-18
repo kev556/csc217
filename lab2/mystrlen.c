353c8c3c8082e32786244863750967cd5524730e
kev556 <26943287+kev556@users.noreply.github.com>
2024-04-17 23:40:05 -0400

    empty commit

--- lab2/mystrlen.c
+++ lab2/mystrlen.c
@@ -5,7 +5,7 @@
 /* Loops through the string pointer until the pointer points to null. 
  subtracting the end memory address from the beginning results in the length of the string*/
 unsigned long long mystrlen(const char *s) {
-    const char *t = s;
-    while (*s++);
-    return s-t;
+	const char *t = s;
+	while (*s++);
+	return s-t;
 }
