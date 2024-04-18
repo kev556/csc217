9f6566d010ae4f1ffc93c81aaae7798853ec11a6
kev556 <26943287+kev556@users.noreply.github.com>
2024-04-17 21:56:06 -0400

    changed datatype of arr

--- lab2/csc217lab2_KevinLi.c
+++ lab2/csc217lab2_KevinLi.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "user.h"
 #include "cli.h"
 
 int main(int argc, char **argv) {
-    char **arr;
+    char *arr[5];
     int maxidx = 0, max = 0;
 
     if (argc == 1)
-        arr = enterInput(&max, &maxidx);
+        userInput(arr, &max, &maxidx);
     else if (argc == 6)
-        arr = cliInput(argc - 1, argv, &max, &maxidx);
+        cliInput(argc, argv, arr, &max, &maxidx);
     else {
         fprintf(stderr, "Incorrect number of cli arguments, please enter 5 strings.\n");
         exit(1);
     }
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++) 
         printf("%s\n", arr[i]);
-
+        
     printf("The largest string entered is [ %s ] with a length of %d \n", arr[maxidx], max);
 
     for (int i = 0; i < 5; i++)
