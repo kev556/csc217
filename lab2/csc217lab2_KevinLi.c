31b5066a608c30401794b92ca73e524d3cdf600d
kev556 <26943287+kev556@users.noreply.github.com>
2024-04-25 17:17:14 -0400

    removed debug print statements

--- lab2/csc217lab2_KevinLi.c
+++ lab2/csc217lab2_KevinLi.c
@@ -7,8 +7,6 @@ int main(int argc, char **argv) {
     char **arr = malloc(102 * 5);
     char *max = malloc(102);
     int maxlen = 0;
-    char *arr[5]; // array of string pointers
-    int maxidx = 0, max = 0; // maxidx will store the index of the largest string in the array. max will store the largest string length
 
     if (argc == 1)
         userInput(arr, &max, &maxlen);
@@ -19,8 +17,6 @@ int main(int argc, char **argv) {
         exit(1);
     }
     for (int i = 0; i < 5; i++) {
-        printf("about to print %s\n", *arr);
-        printf("printed %s\n", *arr);
         free(*arr);
         *arr++;
     }
