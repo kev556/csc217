187bf2330989b12c1547342760d3a6ad71e31d11
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-10 18:58:45 -0400

    completed default switch case

--- proj/proj.c
+++ proj/proj.c
@@ -14,14 +14,21 @@ int main(int argc, char **argv){
 
 	while ((opt = getopt(argc, argv, "hf:")) != -1) {
 		switch (opt) {
+
 			case 'h':
 				printf("");
 				return 0;
 			case 'f':
 				readFromFile(head, fopen(optarg, "r"));
-				
-				
-
+			default:
+				char *word = malloc(80);
+				while (fgets(word, 80, stdin) != EOF) {
+					Node *temp = malloc(sizeof(Node));
+					temp->data = word;
+					temp->next = head;
+					head = temp;
+				}
+				printAll(head);
 		}
 	} 
 }
