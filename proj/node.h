4b96dee97cfe23570432342113593bf00af081f0
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-10 19:05:26 -0400

    added missing data types in struct

--- proj/node.h
+++ proj/node.h
@@ -1,6 +1,9 @@
 #ifndef NODE_H_
 #define NODE_H_
-	typedef struct node {} Node;
+	typedef struct node {
+		char *data;
+		Node next;
+	} Node;
 	void freeList(Node *head);
 	void printAll(Node *head);
 #endif
