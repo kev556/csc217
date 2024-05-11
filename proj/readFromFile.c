ef136302a405ff866d60465d948c6d9f40ff5d83
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-11 19:40:04 -0400

    created readFromFile function and header

---
+++ proj/readFromFile.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+
+void readFromFile(Node** head) {
+	
+}
---
+++ proj/readFromFile.h
@@ -0,0 +1,6 @@
+#include "node.h"
+
+#ifndef READFROMFILE_H_
+#define READFROMFILE_H_
+	void readFromFile(Node** head);
+#endif
