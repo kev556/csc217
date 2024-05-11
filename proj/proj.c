e857b522187d966514431d60d2c0041df6e80199
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-10 20:36:04 -0400

    moved erroneous default case outside

--- proj/proj.c
+++ proj/proj.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <node.h>
+#include "node.h"
 
 
 int main(int argc, char **argv){
 	Node *head = NULL;
 
 	int opt;
-
+	if (argc == 1) {
+		char *word = malloc(80);
+		while (*(fgets(word, 80, stdin)) != EOF) {
+			Node *temp = malloc(sizeof(Node));
+			temp->data = malloc(80);
+			temp->data = word;
+			temp->next = head;
+			head = temp;
+		}
+		free(word);
+		printAll(head);
+		return 0;
+	}
 	while ((opt = getopt(argc, argv, "hf:")) != -1) {
 		switch (opt) {
-
 			case 'h':
-				printf("");
+				printf("hi");
 				return 0;
 			case 'f':
-				readFromFile(head, fopen(optarg, "r"));
+				printf("hi");
 			default:
-				char *word = malloc(80);
-				while (fgets(word, 80, stdin) != EOF) {
-					Node *temp = malloc(sizeof(Node));
-					temp->data = malloc(80);
-					temp->data = word;
-					temp->next = head;
-					head = temp;
-				}
-				free(word);
-				printAll(head);
+				break;
 		}
 	} 
 }
