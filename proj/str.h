0b6f1bfe1de8df1b2a2b71f48e05266355f188f3
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-12 20:59:33 -0400

    added isdigit

--- proj/str.h
+++ proj/str.h
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
 #ifndef STR_H_
 #define STR_H_
 	size_t mystrlen(const char *str);
 	void mystrncpy(char *dest, const char *src, size_t lim);
-	char **mysplit(char *str, char delim);
+	bool isdigit(char c);
 #endif
