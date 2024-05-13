7292a07ed9620bd5f8c46cb78190883090850fb4
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-13 18:27:29 -0400

    added arbitrary space

--- proj/str.h
+++ proj/str.h
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+
 #ifndef STR_H_
 #define STR_H_
 	size_t mystrlen(const char *str);
 	void mystrncpy(char *dest, const char *src, size_t lim);
-	bool isdigit(char c);
+	bool myisdigit(char c);
 #endif
