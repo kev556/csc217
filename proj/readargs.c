fbee95cd2d9249be5b17e20322eaa8f20b8587cc
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-15 01:51:32 -0400

    fixed while loop and changed varnames

--- proj/readargs.c
+++ proj/readargs.c
@@ -6,16 +6,17 @@
 
 void readArgs(int argc, char **argv, char **filearg, int *limit, char **delete) {
 	int i = 1;
-	while (i < argc){ // We will access memory in i + 1, comparing with argc - 1 makes certain we do not go out of bounds
-		if (mystrcmp(*(argv + i), "-h") == 0){
-			printHelp(*argv);
-			exit(EXIT_SUCCESS);
-		}
-		else if (mystrcmp(*(argv + i), "-f") == 0){
-			mystrcpy(*filearg,*(argv + i + 1));
+	if (mystrcmp(*(argv + i), "-h") == 0) {
+		printHelp(*argv);
+		exit(EXIT_SUCCESS);
+	}
+	while (i < argc - 1) { // We will access memory in i + 1, comparing with argc - 1 makes certain we do not go out of bounds
+		if (mystrcmp(*(argv + i), "-f") == 0) {
+			*filearg = malloc(80);
+			mystrncpy(*filearg,*(argv + i + 1), 80);
 			i += 2;
 		}
-		else if (mystrcmp(*(argv + i), "-n") == 0){
+		else if (mystrcmp(*(argv + i), "-n") == 0) {
 			if (myisdigit(**(argv + i + 1))) {
 				*limit = myatoi(*(argv + i + 1));
 				i += 2;
@@ -26,7 +27,12 @@ void readArgs(int argc, char **argv, char **filearg, int *limit, char **delete) 
 			}
 		}
 		else if (mystrcmp(*(argv + i), "-d") == 0){
-			mystrcpy(*delete, *(argv + i + 1));
+			if (i + 1 >= argc) {
+				fprintf(stderr, "No argument for -d. Exiting Program.\n");
+				exit(EXIT_FAILURE);
+			}
+			*delete = malloc(80);
+			mystrncpy(*delete, *(argv + i + 1), 80);
 			i += 2;
 		}
 		else { // From the second arg onwards must be either -h, -f, -n, or -d. if not, exits with error
