39fe185b040ea2d36db218ed1957799c90b9da5a
kev556 <26943287+kev556@users.noreply.github.com>
2024-05-15 01:52:19 -0400

    introduced new condition

--- proj/fileinput.c
+++ proj/fileinput.c
@@ -14,13 +14,13 @@ void getFileInput(Node **stack, Node **queue, FILE *fp, int limit) {
         char* wordPtr = word; // Keeps track of the end of the currently processing word
         char* startPtr = word; // Keeps track of the beginning of the currently processing word
         while (*wordPtr) { // Goes through the current line of the file. 
-            if (*wordPtr == ' ' || *wordPtr == '\n') {
+            if (*wordPtr == ' ' || *wordPtr == '\n' || *wordPtr == '\r') {
                 *wordPtr = '\0'; // Replaces the space or \n with a null terminator
                 *stack = push(*stack, startPtr); // Pushes startPtr, doing this passes the string of all chars between startPtr and \0
                 *queue = enqueue(*queue, startPtr); 
                 startPtr = wordPtr + 1; // Moves startPtr to the start of the next word
             }
-            wordPtr++; // increments wordPtr until it hits a space or newline, which marks the end of the current word
+			wordPtr++; // increments wordPtr until it hits a space or newline, which marks the end of the current word
 			count++;
         }
         *stack = push(*stack, startPtr); // The code written above skips the last word due to the null escape character at the end of a file.
